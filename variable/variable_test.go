bddef74ee78ea95223c87d38a6c0be9d9bf57e81
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-15 10:06:06 +0000

    Add test

--- variable/variable_test.go
+++ variable/variable_test.go
@@ -1,1 +1,15 @@
 package variable_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/autograd/variable"
+)
+
+func ExampleVariable() {
+	v := variable.New(1, 2, 3, 4)
+	fmt.Println(v)
+
+	// Output:
+	// variable([1 2 3 4])
+}
