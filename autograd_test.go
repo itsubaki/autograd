863ae1cf72bcf9a26bb4ec70b89a4ffa92e2388a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-15 10:33:13 +0000

    Update test

--- autograd_test.go
+++ autograd_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/itsubaki/autograd/variable"
 )
 
-func ExampleVariable() {
+func Example() {
 	x := variable.New(0.5)
 	y := F.Square(F.Exp(F.Square(x)))
 
@@ -16,9 +16,10 @@ func ExampleVariable() {
 
 	// Output:
 	// [3.297442541400256]
+
 }
 
-func ExampleVariable_func() {
+func Example_func() {
 	// p44
 	x := variable.New(0.5)
 	a := F.Square(x)
@@ -30,9 +31,10 @@ func ExampleVariable_func() {
 
 	// Output:
 	// [3.297442541400256]
+
 }
 
-func ExampleVariable_type() {
+func Example_type() {
 	// p44
 	A := &F.SquareT{}
 	B := &F.ExpT{}
@@ -48,4 +50,42 @@ func ExampleVariable_type() {
 
 	// Output:
 	// [3.297442541400256]
+
+}
+
+func Example_creator() {
+	// p16
+	A := &F.SquareT{}
+	B := &F.ExpT{}
+	C := &F.SquareT{}
+
+	x := variable.New(0.5)
+	a := A.Apply(x)
+	b := B.Apply(a)
+	y := C.Apply(b)
+
+	fmt.Println(y)
+
+	b.Grad = C.Backward([]float64{1})
+	a.Grad = B.Backward(b.Grad)
+	x.Grad = A.Backward(a.Grad)
+
+	fmt.Println(x.Grad)
+
+	// p40
+	fmt.Println(y.Creator.Input() == b)
+	fmt.Println(y.Creator.Input().Creator == B)
+	fmt.Println(y.Creator.Input().Creator.Input() == a)
+	fmt.Println(y.Creator.Input().Creator.Input().Creator == A)
+	fmt.Println(y.Creator.Input().Creator.Input().Creator.Input() == x)
+
+	// Output:
+	// variable([1.648721270700128])
+	// [3.297442541400256]
+	// true
+	// true
+	// true
+	// true
+	// true
+
 }
--- function/square_test.go
+++ function/square_test.go
@@ -9,6 +9,18 @@ import (
 
 func ExampleSquare() {
 	v := variable.New(1, 2, 3, 4, 5)
+	y := F.Square(v)
+	y.Backward()
+
+	fmt.Println(v.Grad)
+
+	// Output:
+	// [2 4 6 8 10]
+
+}
+
+func ExampleSquareT() {
+	v := variable.New(1, 2, 3, 4, 5)
 	fmt.Println(v)
 
 	f := F.SquareT{}
