cb5bbe92ac4057bb52647977e6b1a2cbd49fbfc3
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-17 03:52:44 +0000

    Update AddT

--- function/add.go
+++ function/add.go
@@ -9,14 +9,14 @@ type AddT struct {
 	x []variable.Data
 }
 
-func (f *AddT) Forward(x ...variable.Data) variable.Data {
+func (f *AddT) Forward(x ...variable.Data) []variable.Data {
 	f.x = x
 
 	add := func(a, b float64) float64 { return a + b }
 	y := vector.F2(x[0], x[1], add)
-	return y
+	return []variable.Data{y}
 }
 
-func (f *AddT) Backward(gy []variable.Data) []variable.Data {
+func (f *AddT) Backward(gy ...variable.Data) []variable.Data {
 	return []variable.Data{gy[0], gy[0]}
 }
