27bacf3ffad38ded964e991d2756681f96067fbf
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-17 05:31:11 +0000

    Update Add

--- function/add.go
+++ function/add.go
@@ -9,15 +9,11 @@ func Add(x ...*variable.Variable) []*variable.Variable {
 	return (&Function{Forwarder: &AddT{}}).Apply(x...)
 }
 
-type AddT struct {
-	x []variable.Data
-}
+type AddT struct{}
 
 func (f *AddT) Forward(x ...variable.Data) []variable.Data {
-	f.x = x
-
 	add := func(a, b float64) float64 { return a + b }
-	y := vector.F2(f.x[0], f.x[1], add)
+	y := vector.F2(x[0], x[1], add)
 	return []variable.Data{y}
 }
 
