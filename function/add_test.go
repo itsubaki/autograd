e65bdcf2c44593db715440d1f3448a46946426a9
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-17 03:52:44 +0000

    Update AddT

--- function/add_test.go
+++ function/add_test.go
@@ -16,11 +16,11 @@ func ExampleAddT() {
 
 	f := F.AddT{}
 	fmt.Println(f.Forward(v.Data, w.Data))
-	fmt.Println(f.Backward([]variable.Data{vector.OneLike(v.Data)}))
+	fmt.Println(f.Backward([]variable.Data{vector.OneLike(v.Data)}...))
 
 	// Output:
 	// variable([2 3])
 	// variable([3 4])
-	// [5 7]
+	// [[5 7]]
 	// [[1 1] [1 1]]
 }
