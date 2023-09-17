2ca36c21da3cb8d73b8d9f4c5db1aa594f8d8b0d
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-17 13:20:09 +0000

    Add Sub

---
+++ function/sub_test.go
@@ -0,0 +1,25 @@
+package function_test
+
+import (
+	"fmt"
+
+	F "github.com/itsubaki/autograd/function"
+	"github.com/itsubaki/autograd/variable"
+)
+
+func ExampleSub() {
+	v := variable.New(2, 3)
+	w := variable.New(3, 4)
+
+	y := F.Sub(v, w)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(v.Grad)
+	fmt.Println(w.Grad)
+
+	// Output:
+	// variable([-1 -1])
+	// [1 1]
+	// [-1 -1]
+}
--- vector/vector.go
+++ vector/vector.go
@@ -16,8 +16,16 @@ func SubC(v []float64, c float64) []float64 {
 	return F(v, func(a float64) float64 { return a - c })
 }
 
+func MulC(v []float64, c float64) []float64 {
+	return F(v, func(a float64) float64 { return c * a })
+}
+
 func Add(v, w []float64) []float64 {
 	return F2(v, w, func(a, b float64) float64 { return a + b })
+}
+
+func Sub(v, w []float64) []float64 {
+	return F2(v, w, func(a, b float64) float64 { return a - b })
 }
 
 func F(v []float64, f func(a float64) float64) []float64 {
