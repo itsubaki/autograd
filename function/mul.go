5ad409e554fa919a1fbb0d6d33304ed8999f2500
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-18 14:45:09 +0000

    Update Mul Backward

--- function/mul.go
+++ function/mul.go
@@ -16,15 +16,13 @@ type MulT struct {
 func (f *MulT) Forward(x ...variable.Data) []variable.Data {
 	f.x0, f.x1 = x[0], x[1]
 
-	y := vector.F2(f.x0, f.x1, mul)
+	y := vector.Mul(f.x0, f.x1)
 	return []variable.Data{y}
 }
 
 func (f *MulT) Backward(gy ...*variable.Variable) []*variable.Variable {
 	return []*variable.Variable{
-		variable.New(vector.F2(f.x1, gy[0].Data, mul)...),
-		variable.New(vector.F2(f.x0, gy[0].Data, mul)...),
+		Mul(variable.New(f.x1...), gy[0]),
+		Mul(variable.New(f.x0...), gy[0]),
 	}
 }
-
-func mul(a, b float64) float64 { return a * b }
