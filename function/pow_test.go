6b6d57b3b624b6563527d5bf5f1a3f8a04789195
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-18 15:57:54 +0000

    Add higher derivative of pow

--- function/pow_test.go
+++ function/pow_test.go
@@ -8,14 +8,37 @@ import (
 )
 
 func ExamplePow() {
-	v := variable.New(2.0)
-	y := F.Pow(3.0)(v)
+	x := variable.New(2.0)
+	y := F.Pow(3.0)(x)
 	y[0].Backward()
 
 	fmt.Println(y)
-	fmt.Println(v.Grad)
+	fmt.Println(x.Grad)
 
 	// Output:
 	// [variable[8]]
 	// variable[12]
+}
+
+func ExamplePow_higher() {
+	x := variable.New(2.0)
+	y := F.Pow(3.0)(x)
+	y[0].Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	for i := 0; i < 3; i++ {
+		gx := x.Grad
+		x.Cleargrad()
+		gx.Backward()
+		fmt.Println(x.Grad)
+	}
+
+	// Output:
+	// [variable[8]]
+	// variable[12]
+	// variable[12]
+	// variable[6]
+	// variable[0]
 }
