20472a6cb20c4ed4a6df55add0141cbd81be78cc
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-25 00:10:24 +0900

    Update dot

--- dot/dot_test.go
+++ dot/dot_test.go
@@ -15,18 +15,17 @@ func ExampleVar() {
 	x := variable.New(1)
 	x.Name = "x"
 
-	dotx := dot.Var(x)
-	fmt.Println(re.ReplaceAllString(dotx, "**********"))
-	fmt.Println(dotx == dot.Var(x))
+	fmt.Println(re.ReplaceAllString(dot.Var(x), "**********"))
+	fmt.Println(re.ReplaceAllString(dot.Var(x, dot.Opt{Verbose: true}), "**********"))
+	fmt.Println(dot.Var(x) == dot.Var(x))
 
 	y := variable.New(1)
-	y.Name = "y"
-
-	doty := dot.Var(y)
-	fmt.Println(dotx == doty)
+	y.Name = "x"
+	fmt.Println(dot.Var(x) == dot.Var(y))
 
 	// Output:
 	// "0x**********" [label="x", color=orange, style=filled]
+	// "0x**********" [label="x[1]", color=orange, style=filled]
 	// true
 	// false
 }
@@ -82,6 +81,34 @@ func ExampleGraph() {
 
 	// Output:
 	// digraph g {
+	// "0x**********" [label="y", color=orange, style=filled]
+	// "0x**********" [label="SinT", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
+	// "0x**********" [label="x", color=orange, style=filled]
+	// }
+}
+
+func ExampleGraph_composite() {
+	var re = regexp.MustCompile("[0-9a-f]{10}")
+
+	x := variable.New(1.0)
+	y := F.Sin(x)
+	z := F.Cos(y)
+	x.Name = "x"
+	y.Name = "y"
+	z.Name = "z"
+
+	for _, txt := range dot.Graph(z) {
+		fmt.Println(re.ReplaceAllString(txt, "**********"))
+	}
+
+	// Output:
+	// digraph g {
+	// "0x**********" [label="z", color=orange, style=filled]
+	// "0x**********" [label="CosT", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
 	// "0x**********" [label="y", color=orange, style=filled]
 	// "0x**********" [label="SinT", color=lightblue, style=filled, shape=box]
 	// "0x**********" -> "0x**********"
