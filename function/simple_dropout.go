901ae50d251af876db46402e49c00ead0111cfbc
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-09 00:58:23 +0900

    Add Dropout

---
+++ function/simple_dropout.go
@@ -0,0 +1,24 @@
+package function
+
+import (
+	"math/rand"
+
+	"github.com/itsubaki/autograd/matrix"
+	"github.com/itsubaki/autograd/variable"
+)
+
+func Dropout(ratio float64, s ...rand.Source) func(x ...*variable.Variable) *variable.Variable {
+	return func(x ...*variable.Variable) *variable.Variable {
+		if !variable.Config.Train {
+			return x[0]
+		}
+
+		xs := x[0].Shape()
+		mask := matrix.Mask(matrix.Rand(xs[0], xs[1], s...), mask(ratio))
+		return MulC(1.0/(1.0-ratio), Mul(x[0], variable.NewOf(mask...))) // y = x * mask / (1 - ratio)
+	}
+}
+
+func mask(ratio float64) func(v float64) bool {
+	return func(v float64) bool { return v > ratio }
+}
---
+++ function/simple_dropout_test.go
@@ -0,0 +1,43 @@
+package function_test
+
+import (
+	"fmt"
+	"math/rand"
+
+	F "github.com/itsubaki/autograd/function"
+	"github.com/itsubaki/autograd/variable"
+)
+
+func ExampleDropout() {
+	// p436
+	s := rand.NewSource(1)
+
+	x := variable.New(1, 1, 1, 1, 1)
+	y := F.Dropout(0.5, s)(x)
+	fmt.Println(y)
+
+	func() {
+		defer variable.TestMode().End()
+
+		y := F.Dropout(0.5)(x)
+		fmt.Println(y)
+	}()
+
+	// Output:
+	// variable([2 2 2 0 0])
+	// variable([1 1 1 1 1])
+}
+
+func ExampleDropout_backward() {
+	s := rand.NewSource(1)
+	x := variable.New(0.1, 0.2, 0.3, 0.4, 0.5)
+	y := F.Dropout(0.5, s)(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable([0.2 0.4 0.6 0 0])
+	// variable([2 2 2 0 0])
+}
