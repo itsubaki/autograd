076a482498549438be0334390da8fde76b73700a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-08 19:54:19 +0900

    Update some files

--- function/sigmoid.go
+++ function/sigmoid.go
@@ -14,8 +14,8 @@ type SigmoidT struct {
 }
 
 func (f *SigmoidT) Forward(x ...*variable.Variable) []*variable.Variable {
-	s := matrix.AddC(0.5, matrix.MulC(0.5, matrix.Tanh(matrix.MulC(0.5, x[0].Data)))) // 0.5 + 0.5 * tanh(0.5 * x)
-	f.y = variable.NewOf(s...)
+	v := matrix.AddC(0.5, matrix.MulC(0.5, matrix.Tanh(matrix.MulC(0.5, x[0].Data)))) // 0.5 + 0.5 * tanh(0.5 * x)
+	f.y = variable.NewOf(v...)
 	return []*variable.Variable{
 		f.y,
 	}
--- function/simple_sigmoid.go
+++ function/simple_sigmoid.go
@@ -3,5 +3,5 @@ package function
 import "github.com/itsubaki/autograd/variable"
 
 func SigmoidSimple(x *variable.Variable) *variable.Variable {
-	return DivC(1.0, AddC(1.0, Exp(Neg(x)))) // 1 / (1 + exp(-x))
+	return DivC(1.0, AddC(1.0, Exp(Neg(x)))) // y = 1 / (1 + exp(-x))
 }
