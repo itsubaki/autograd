330c98a98bc7bc74e250b8a4b07e893462db623c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-08 19:18:42 +0900

    Update SoftmaxSimple

--- function/simple_softmax_test.go
+++ function/simple_softmax_test.go
@@ -23,3 +23,22 @@ func ExampleSoftmaxSimple() {
 	// variable([[0.09003057317038046 0.24472847105479767 0.6652409557748219] [0.017668422014048047 0.017668422014048047 0.9646631559719039]])
 	// variable([[0 0 0] [0 0 0]])
 }
+
+func Example_softmax1d() {
+	softmax1d := func(x *variable.Variable) *variable.Variable {
+		y := F.Exp(x)
+		sumy := F.Sum(y)
+		return F.Div(y, sumy)
+	}
+
+	x := variable.New(0.2, -0.4)
+	y := softmax1d(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable([0.6456563062257954 0.3543436937742045])
+	// variable([0 0])
+}
