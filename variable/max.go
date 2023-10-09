4501b9104f1a86023bb1b23e789a42a8fb3b63ad
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-09 21:33:30 +0900

    Add Max, Min

---
+++ variable/max.go
@@ -0,0 +1,39 @@
+package variable
+
+import "github.com/itsubaki/autograd/matrix"
+
+func Max(x ...*Variable) *Variable {
+	return (&Function{Forwarder: &MaxT{}}).ApplyAndFirst(x...)
+}
+
+type MaxT struct {
+	x, y *Variable
+}
+
+func (f *MaxT) Forward(x ...*Variable) []*Variable {
+	f.x = x[0]
+
+	f.y = NewOf(matrix.Max(x[0].Data)...)
+	return []*Variable{
+		f.y,
+	}
+}
+
+func (f *MaxT) Backward(gy ...*Variable) []*Variable {
+	ybr := matrix.BroadcastTo(f.x.Shape(), f.y.Data)
+	cond := NewOf(matrix.F2(f.x.Data, ybr, cond)...)
+	gybr := BroadcastTo(cond.Shape()...)(gy[0])
+
+	gx := Mul(gybr, cond) // argmax(gy)
+	return []*Variable{
+		gx,
+	}
+}
+
+func cond(a, b float64) float64 {
+	if a == b {
+		return 1.0
+	}
+
+	return 0.0
+}
---
+++ variable/min_test.go
@@ -0,0 +1,24 @@
+package variable_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/autograd/variable"
+)
+
+func ExampleMin() {
+	A := variable.NewOf(
+		[]float64{1, 2, 3},
+		[]float64{4, -5, 6},
+	)
+
+	y := variable.Min(A)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(A.Grad)
+
+	// Output:
+	// variable([-5])
+	// variable([[0 0 0] [0 1 0]])
+}
