4a7ed02eb41f35cbf4e87c1f7d314d009d005110
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-12 21:12:03 +0900

    Update some files

--- function/simple_mean_squared_error.go
+++ function/simple_mean_squared_error.go
@@ -4,6 +4,6 @@ import "github.com/itsubaki/autograd/variable"
 
 func MeanSquaredErrorSimple(x0, x1 *variable.Variable) *variable.Variable {
 	diff := Sub(x0, x1)
-	N := float64(len(diff.Data))
-	return MulC(1.0/N, Sum(Mul(diff, diff)))
+	N := len(diff.Data)
+	return MulC(1.0/float64(N), Sum(Mul(diff, diff)))
 }
