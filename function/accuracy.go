944f64d1e43f23a6de4e7cfa1fe92a5e2ecd463d
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-10-25 00:06:41 +0900

    Update isclose

--- function/accuracy.go
+++ function/accuracy.go
@@ -12,12 +12,14 @@ import (
 func Accuracy(y, t *variable.Variable) *variable.Variable {
 	argmax := matrix.From([][]int{matrix.Argmax(y.Data)})
 	pred := matrix.Reshape(matrix.Shape(t.Data), argmax)
-	result := matrix.F2(pred, t.Data, acc)
+	result := matrix.F2(pred, t.Data, isclose)
 	return variable.New(matrix.Mean(result))
+
 }
 
-func acc(a, b float64) float64 {
-	if math.Abs(a-b) < 1e-13 {
+func isclose(a, b float64) float64 {
+	atol, rtol := 1e-08, 1e-05
+	if math.Abs(a-b) < atol+rtol*math.Abs(b) {
 		return 1
 	}
 
