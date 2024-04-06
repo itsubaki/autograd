b1976e60a33b2a9611856b020edd656191d1f40f
itsubaki <1759459+itsubaki@users.noreply.github.com>
2024-04-06 05:05:54 +0000

    Update rand/source

--- rand/source.go
+++ rand/source.go
@@ -2,21 +2,21 @@ package rand
 
 import (
 	crand "crypto/rand"
-	"math/rand/v2"
+	randv2 "math/rand/v2"
 )
 
 // NewSource returns a source of pseudo-random number generator
-func NewSource() rand.Source {
+func NewSource() randv2.Source {
 	var p [32]byte
 	if _, err := crand.Read(p[:]); err != nil {
 		panic(err)
 	}
 
-	return rand.NewChaCha8(p)
+	return randv2.NewChaCha8(p)
 }
 
 // Const returns a source of constant pseudo-random number generator
-func Const(seed ...uint64) rand.Source {
+func Const(seed ...uint64) randv2.Source {
 	var s0, s1 uint64
 	if len(seed) > 0 {
 		s0 = seed[0]
@@ -26,5 +26,5 @@ func Const(seed ...uint64) rand.Source {
 		s1 = seed[1]
 	}
 
-	return rand.NewPCG(s0, s1)
+	return randv2.NewPCG(s0, s1)
 }
