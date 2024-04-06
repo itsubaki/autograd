86e509ee4bf21d54a9af27e1c575137386730159
itsubaki <1759459+itsubaki@users.noreply.github.com>
2024-04-06 14:41:07 +0000

    Add test

--- rand/source_test.go
+++ rand/source_test.go
@@ -52,3 +52,21 @@ func TestConst(t *testing.T) {
 
 	t.Fail()
 }
+
+func TestMustPanic(t *testing.T) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			err, ok := rec.(error)
+			if !ok {
+				t.Fail()
+			}
+
+			if err.Error() != "something went wrong" {
+				t.Fail()
+			}
+		}
+	}()
+
+	rand.Must(-1, fmt.Errorf("something went wrong"))
+	t.Fail()
+}
