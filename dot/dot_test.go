4794820890aec6ba06b2cdc87193a396c53941b0
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-18 00:07:45 +0900

    Fix dot test

--- dot/dot_test.go
+++ dot/dot_test.go
@@ -9,14 +9,16 @@ import (
 	"github.com/itsubaki/autograd/variable"
 )
 
-func ExampleVar() {
-	var re = regexp.MustCompile("[0-9a-f]{11}")
+var re = regexp.MustCompile(`0x[0-9a-fA-F]+`)
 
+const dummyAddr = "0x**********"
+
+func ExampleVar() {
 	x := variable.New(1)
 	x.Name = "x"
 
-	fmt.Println(re.ReplaceAllString(dot.Var(x), "***********"))
-	fmt.Println(re.ReplaceAllString(dot.Var(x, dot.Opts{Verbose: true}), "***********"))
+	fmt.Println(re.ReplaceAllString(dot.Var(x), dummyAddr))
+	fmt.Println(re.ReplaceAllString(dot.Var(x, dot.Opts{Verbose: true}), dummyAddr))
 
 	// It always returns the same value.
 	xvar0, xvar1 := dot.Var(x), dot.Var(x)
@@ -27,31 +29,27 @@ func ExampleVar() {
 	fmt.Println(dot.Var(x) == dot.Var(y))
 
 	// Output:
-	// "0x***********" [label="x", color=orange, style=filled]
-	// "0x***********" [label="x([1])", color=orange, style=filled]
+	// "0x**********" [label="x", color=orange, style=filled]
+	// "0x**********" [label="x([1])", color=orange, style=filled]
 	// true
 	// false
 }
 
 func ExampleFunc() {
-	var re = regexp.MustCompile("[0-9a-f]{11}")
-
 	f0 := &variable.Function{Forwarder: &variable.SinT{}}
 	for _, txt := range dot.Func(f0) {
-		fmt.Println(re.ReplaceAllString(txt, "***********"))
+		fmt.Println(re.ReplaceAllString(txt, dummyAddr))
 	}
 
 	f1 := &variable.Function{Forwarder: &variable.SinT{}}
 	fmt.Println(dot.Func(f0)[0] == dot.Func(f1)[0])
 
 	// Output:
-	// "0x***********" [label="Sin", color=lightblue, style=filled, shape=box]
+	// "0x**********" [label="Sin", color=lightblue, style=filled, shape=box]
 	// false
 }
 
 func Example_func() {
-	var re = regexp.MustCompile("[0-9a-f]{11}")
-
 	f := &variable.Function{
 		Input:     []*variable.Variable{variable.New(1)},
 		Output:    []*variable.Variable{variable.New(1)},
@@ -59,18 +57,16 @@ func Example_func() {
 	}
 
 	for _, txt := range dot.Func(f) {
-		fmt.Println(re.ReplaceAllString(txt, "***********"))
+		fmt.Println(re.ReplaceAllString(txt, dummyAddr))
 	}
 
 	// Output:
-	// "0x***********" [label="Sin", color=lightblue, style=filled, shape=box]
-	// "0x***********" -> "0x***********"
-	// "0x***********" -> "0x***********"
+	// "0x**********" [label="Sin", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
 }
 
 func ExampleGraph() {
-	var re = regexp.MustCompile("[0-9a-f]{11}")
-
 	x := variable.New(1.0)
 	x.Name = "x"
 
@@ -78,22 +74,20 @@ func ExampleGraph() {
 	y.Name = "y"
 
 	for _, txt := range dot.Graph(y) {
-		fmt.Println(re.ReplaceAllString(txt, "***********"))
+		fmt.Println(re.ReplaceAllString(txt, dummyAddr))
 	}
 
 	// Output:
 	// digraph g {
-	// "0x***********" [label="y", color=orange, style=filled]
-	// "0x***********" [label="Sin", color=lightblue, style=filled, shape=box]
-	// "0x***********" -> "0x***********"
-	// "0x***********" -> "0x***********"
-	// "0x***********" [label="x", color=orange, style=filled]
+	// "0x**********" [label="y", color=orange, style=filled]
+	// "0x**********" [label="Sin", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
+	// "0x**********" [label="x", color=orange, style=filled]
 	// }
 }
 
 func ExampleGraph_composite() {
-	var re = regexp.MustCompile("[0-9a-f]{11}")
-
 	x := variable.New(1.0)
 	y := F.Sin(x)
 	z := F.Cos(y)
@@ -102,20 +96,20 @@ func ExampleGraph_composite() {
 	z.Name = "z"
 
 	for _, txt := range dot.Graph(z) {
-		fmt.Println(re.ReplaceAllString(txt, "***********"))
+		fmt.Println(re.ReplaceAllString(txt, dummyAddr))
 	}
 
 	// Output:
 	// digraph g {
-	// "0x***********" [label="z", color=orange, style=filled]
-	// "0x***********" [label="Cos", color=lightblue, style=filled, shape=box]
-	// "0x***********" -> "0x***********"
-	// "0x***********" -> "0x***********"
-	// "0x***********" [label="y", color=orange, style=filled]
-	// "0x***********" [label="Sin", color=lightblue, style=filled, shape=box]
-	// "0x***********" -> "0x***********"
-	// "0x***********" -> "0x***********"
-	// "0x***********" [label="x", color=orange, style=filled]
+	// "0x**********" [label="z", color=orange, style=filled]
+	// "0x**********" [label="Cos", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
+	// "0x**********" [label="y", color=orange, style=filled]
+	// "0x**********" [label="Sin", color=lightblue, style=filled, shape=box]
+	// "0x**********" -> "0x**********"
+	// "0x**********" -> "0x**********"
+	// "0x**********" [label="x", color=orange, style=filled]
 	// }
 }
 
