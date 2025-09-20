19f869d8c8fddd0ae223c22b1812e682e4cc48b9
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-20 13:57:52 +0000

    Add test

--- variable/sum_to_test.go
+++ variable/sum_to_test.go
@@ -17,10 +17,11 @@ func ExampleSumTo() {
 
 	fmt.Println(y)
 	fmt.Println(x.Grad)
-	x.Cleargrad()
 
+	x.Cleargrad()
 	y = variable.SumTo(2, 1)(x)
 	y.Backward()
+
 	fmt.Println(y)
 	fmt.Println(x.Grad)
 
@@ -28,5 +29,21 @@ func ExampleSumTo() {
 	// variable[1 3]([5 7 9])
 	// variable[2 3]([1 1 1 1 1 1])
 	// variable[2 1]([6 15])
+	// variable[2 3]([1 1 1 1 1 1])
+}
+
+func ExampleSumTo_axes() {
+	x := variable.NewOf(
+		[]float64{1, 2, 3},
+		[]float64{4, 5, 6},
+	)
+	y := variable.SumTo(1)(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable(21)
 	// variable[2 3]([1 1 1 1 1 1])
 }
