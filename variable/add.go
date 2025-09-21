2848f1b285e620e4e38f5c2c4f276109186e2cd5
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-21 14:56:42 +0900

    Update some files

--- variable/add.go
+++ variable/add.go
@@ -40,3 +40,17 @@ func (f *AddT) Backward(gy ...*Variable) []*Variable {
 		SumTo(f.x1Shape...)(gy[0]),
 	}
 }
+
+func equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
