df4631ad89696982e874e5f11909dae9d0d3ca51
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-21 09:50:09 +0000

    Fix Transpose, MatMul

--- variable/matmul_test.go
+++ variable/matmul_test.go
@@ -2,7 +2,9 @@ package variable_test
 
 import (
 	"fmt"
+	"testing"
 
+	"github.com/itsubaki/autograd/tensor"
 	"github.com/itsubaki/autograd/variable"
 )
 
@@ -21,10 +23,101 @@ func ExampleMatMul() {
 	y := variable.MatMul(x, w)
 	y.Backward()
 
-	fmt.Println(x.Grad.Shape())
-	fmt.Println(w.Grad.Shape())
+	fmt.Println(x.Grad.Shape(), x.Grad.Data.Data)
+	fmt.Println(w.Grad.Shape(), w.Grad.Data.Data)
 
 	// Output:
-	// [2 3]
-	// [3 4]
+	// [2 3] [10 26 42 10 26 42]
+	// [3 4] [5 5 5 5 7 7 7 7 9 9 9 9]
+}
+
+func TestMatMul(t *testing.T) {
+	cases := []struct {
+		x, w *variable.Variable
+		y    *variable.Variable
+		gx   *variable.Variable
+		gw   *variable.Variable
+	}{
+		{
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+			).Reshape(2, 3),
+			w: variable.New(
+				1, 2, 3, 4,
+				5, 6, 7, 8,
+				9, 10, 11, 12,
+			).Reshape(3, 4),
+			y: variable.New(
+				38, 44, 50, 56,
+				83, 98, 113, 128,
+			).Reshape(2, 4),
+			gx: variable.New(
+				10, 26, 42,
+				10, 26, 42,
+			).Reshape(2, 3),
+			gw: variable.New(
+				5, 5, 5, 5,
+				7, 7, 7, 7,
+				9, 9, 9, 9,
+			).Reshape(3, 4),
+		},
+		{
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+
+				7, 8, 9,
+				10, 11, 12,
+			).Reshape(2, 2, 3),
+			w: variable.New(
+				1, 2, 3, 4,
+				5, 6, 7, 8,
+				9, 10, 11, 12,
+
+				1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+			).Reshape(2, 3, 4),
+			y: variable.New(
+				38, 44, 50, 56,
+				83, 98, 113, 128,
+
+				7, 8, 9, 0,
+				10, 11, 12, 0,
+			).Reshape(2, 2, 4),
+			gx: variable.New(
+				10, 26, 42,
+				10, 26, 42,
+
+				1, 1, 1,
+				1, 1, 1,
+			).Reshape(2, 2, 3),
+			gw: variable.New(
+				5, 5, 5, 5,
+				7, 7, 7, 7,
+				9, 9, 9, 9,
+
+				17, 17, 17, 17,
+				19, 19, 19, 19,
+				21, 21, 21, 21,
+			).Reshape(2, 3, 4),
+		},
+	}
+
+	for _, c := range cases {
+		y := variable.MatMul(c.x, c.w)
+		if !tensor.IsCloseAll(y.Data, c.y.Data, 1e-8, 1e-5) {
+			t.Errorf("got=%v, want=%v", y, c.y)
+		}
+
+		y.Backward()
+		if !tensor.IsCloseAll(c.x.Grad.Data, c.gx.Data, 1e-8, 1e-5) {
+			t.Errorf("got=%v, want=%v", c.x.Grad, c.gx)
+		}
+
+		if !tensor.IsCloseAll(c.w.Grad.Data, c.gw.Data, 1e-8, 1e-5) {
+			t.Errorf("got=%v, want=%v", c.w.Grad, c.gw)
+		}
+	}
 }
--- variable/transpose_test.go
+++ variable/transpose_test.go
@@ -2,7 +2,9 @@ package variable_test
 
 import (
 	"fmt"
+	"testing"
 
+	"github.com/itsubaki/autograd/tensor"
 	"github.com/itsubaki/autograd/variable"
 )
 
@@ -22,4 +24,155 @@ func ExampleTranspose() {
 	// Output:
 	// variable[3 2]([1 4 2 5 3 6])
 	// variable[2 3]([1 1 1 1 1 1])
+}
+
+func TestTranspose(t *testing.T) {
+	cases := []struct {
+		x    *variable.Variable
+		axes []int
+		y    *variable.Variable
+		gx   *variable.Variable
+	}{
+		{
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+			).Reshape(2, 3),
+			axes: []int{1, 0},
+			y: variable.New(
+				1, 4,
+				2, 5,
+				3, 6,
+			).Reshape(3, 2),
+			gx: variable.New(
+				1, 1, 1,
+				1, 1, 1,
+			).Reshape(2, 3),
+		},
+		{
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+
+				7, 8, 9,
+				10, 11, 12,
+			).Reshape(2, 2, 3),
+			axes: []int{0, 2, 1},
+			y: variable.New(
+				1, 4,
+				2, 5,
+				3, 6,
+
+				7, 10,
+				8, 11,
+				9, 12,
+			).Reshape(2, 3, 2),
+			gx: variable.New(
+				1, 1, 1,
+				1, 1, 1,
+
+				1, 1, 1,
+				1, 1, 1,
+			).Reshape(2, 2, 3),
+		},
+		{
+			// transpose 2 0 1
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+
+				7, 8, 9,
+				10, 11, 12,
+			).Reshape(2, 2, 3),
+			axes: []int{2, 0, 1},
+			y: variable.New(
+				1, 4,
+				7, 10,
+
+				2, 5,
+				8, 11,
+
+				3, 6,
+				9, 12,
+			).Reshape(3, 2, 2),
+			gx: variable.New(
+				1, 1, 1,
+				1, 1, 1,
+
+				1, 1, 1,
+				1, 1, 1,
+			).Reshape(2, 2, 3),
+		},
+		{
+			// traspose -1 -3 -2
+			x: variable.New(
+				1, 2, 3,
+				4, 5, 6,
+
+				7, 8, 9,
+				10, 11, 12,
+			).Reshape(2, 2, 3),
+			axes: []int{-1, -3, -2},
+			y: variable.New(
+				1, 4,
+				7, 10,
+
+				2, 5,
+				8, 11,
+
+				3, 6,
+				9, 12,
+			).Reshape(3, 2, 2),
+			gx: variable.New(
+				1, 1, 1,
+				1, 1, 1,
+
+				1, 1, 1,
+				1, 1, 1,
+			).Reshape(2, 2, 3),
+		},
+	}
+
+	for _, c := range cases {
+		y := variable.Transpose(c.axes...)(c.x)
+		if !tensor.IsCloseAll(y.Data, c.y.Data, 1e-8, 1e-5) {
+			t.Errorf("got=%v, want=%v", y.Data, c.y.Data)
+		}
+
+		y.Backward()
+		if !tensor.IsCloseAll(c.x.Grad.Data, c.gx.Data, 1e-8, 1e-5) {
+			t.Errorf("got=%v, want=%v", c.x.Grad.Data, c.gx.Data)
+		}
+	}
+}
+
+func TestInvPerm_invalid(t *testing.T) {
+	cases := []struct {
+		ndim int
+		axes []int
+	}{
+		{
+			ndim: 3,
+			axes: []int{0, 1},
+		},
+		{
+			ndim: 3,
+			axes: []int{-10, 0, 10},
+		},
+	}
+
+	for _, c := range cases {
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					return
+				}
+
+				t.Errorf("unexpected panic for axes %v", c.axes)
+			}()
+
+			_ = variable.InvPerm(c.ndim, c.axes...)
+			t.Fail()
+		}()
+	}
 }
