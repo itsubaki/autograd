147c744e8ec89c0c330ceec90a066166b89bb59b
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-22 15:28:09 +0000

    Add test

--- variable/sum_to_test.go
+++ variable/sum_to_test.go
@@ -41,3 +41,20 @@ func ExampleSumTo_axes21() {
 	// variable[2 1]([6 15])
 	// variable[2 3]([1 1 1 1 1 1])
 }
+
+func ExampleSumTo_axes11() {
+	x := variable.New(
+		1, 2, 3,
+		4, 5, 6,
+	).Reshape(2, 3)
+
+	y := variable.SumTo(1, 1)(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable(21)
+	// variable[2 3]([1 1 1 1 1 1])
+}
