7177eb553d3d1b6e78d2c20a61535d588b80e0d4
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-23 05:00:39 +0000

    Update some files

--- function/simple_linear.go
+++ function/simple_linear.go
@@ -4,15 +4,14 @@ import "github.com/itsubaki/autograd/variable"
 
 func LinearSimple(x, w *variable.Variable, b ...*variable.Variable) *variable.Variable {
 	t := MatMul(x, w)
-	if len(b) != 0 {
-		y := Add(t, b[0])
-
-		// `t` is not used in backprop for `MatMul` and `Add`.
-		// Clear to save memory.
-		t.Data = nil
-		return y
+	if len(b) == 0 {
+		return t
 	}
 
-	return t
+	y := Add(t, b[0])
 
+	// `t` is not used in backprop for `MatMul` and `Add`.
+	// Clear to save memory.
+	t.Data = nil
+	return y
 }
