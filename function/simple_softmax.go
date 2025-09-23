9b40e650942b65c94f2845250f17ff50dc62f54a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-23 04:49:30 +0000

    Update some files

--- function/simple_softmax.go
+++ function/simple_softmax.go
@@ -3,8 +3,7 @@ package function
 import "github.com/itsubaki/autograd/variable"
 
 func SoftmaxSimple(x *variable.Variable, axis int) *variable.Variable {
-	shape := x.Shape()
-	shape[axis] = 1
+	shape := keepDims(x.Shape(), axis)
 
 	y := Exp(x)
 	sumy := SumTo(shape...)(y)
--- function/softmax.go
+++ function/softmax.go
@@ -31,8 +31,7 @@ func (f *SoftmaxT) Forward(x ...*variable.Variable) []*variable.Variable {
 }
 
 func (f *SoftmaxT) Backward(gy ...*variable.Variable) []*variable.Variable {
-	shape := f.y.Shape()
-	shape[f.Axis] = 1
+	shape := keepDims(f.y.Shape(), f.Axis)
 
 	gyy := Mul(gy[0], f.y)        // gyy = gy * y
 	sum := SumTo(shape...)(gyy)   // sum = sum(gy, axis=1)
@@ -40,4 +39,14 @@ func (f *SoftmaxT) Backward(gy ...*variable.Variable) []*variable.Variable {
 	return []*variable.Variable{
 		gx,
 	}
+}
+
+func keepDims(shape []int, axis int) []int {
+	ndim := len(shape)
+	if axis < 0 {
+		axis += ndim
+	}
+
+	shape[axis] = 1
+	return shape
 }
