62f8a9a3b6cef5a570d239f38191b2a351b779e2
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-24 13:24:46 +0000

    Update Accuracy

--- function/accuracy.go
+++ function/accuracy.go
@@ -8,8 +8,9 @@ import (
 // Accuracy returns the accuracy of the prediction.
 // The return values cannot be backpropagated.
 func Accuracy(y, t *variable.Variable) *variable.Variable {
-	label := tensor.Int(t.Data)
-	pred := tensor.Reshape(tensor.Argmax(y.Data, 1), t.Shape()...)
-	hit := tensor.Equal(pred, label)
-	return variable.From(tensor.Mean(hit))
+	argmax := tensor.Argmax(y.Data, 1)
+	pred := tensor.Reshape(argmax, t.Shape()...)
+	result := tensor.Equal(pred, tensor.Int(t.Data))
+	acc := tensor.Mean(result)
+	return variable.From(acc)
 }
