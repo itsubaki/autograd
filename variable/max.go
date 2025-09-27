8b3b1329a935f6a3dc9905984c0a8b01ecfa666a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-27 22:23:10 +0900

    Update IsClose

--- variable/max.go
+++ variable/max.go
@@ -53,7 +53,7 @@ func (f *MaxT) Backward(gy ...*Variable) []*Variable {
 
 func IsClose(a, b float64) float64 {
 	atol, rtol := 1e-08, 1e-05
-	if math.Abs(a-b) < atol+rtol*math.Abs(b) {
+	if math.Abs(a-b) <= atol+rtol*math.Max(math.Abs(a), math.Abs(b)) {
 		return 1
 	}
 
