a5f7c44d864346e9f286e00d4e111f78c340cc4a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-28 20:49:43 +0900

    Add comment

--- variable/variance.go
+++ variable/variance.go
@@ -26,11 +26,10 @@ func (f *VarianceT) Forward(x ...*Variable) []*Variable {
 
 func (f *VarianceT) Backward(gy ...*Variable) []*Variable {
 	if len(f.Axes) == 0 {
-		mu := Mean(f.Axes...)(f.x)
-		xc := Sub(f.x, mu)
-
-		count := f.x.Data.Size()
-		gx := MulC(2/float64(count), xc)
+		mu := Mean(f.Axes...)(f.x)       // mean(x)
+		xc := Sub(f.x, mu)               // x - mean(x)
+		count := f.x.Data.Size()         // N
+		gx := MulC(2/float64(count), xc) // 2/N * (x - mean(x))
 		return []*Variable{
 			Mul(gy[0], gx),
 		}
@@ -45,10 +44,10 @@ func (f *VarianceT) Backward(gy ...*Variable) []*Variable {
 
 	// mu = mean(x, axes)
 	reshape := tensor.KeepDims(shape, f.Axes)
-	mu := Mean(f.Axes...)(f.x)
-	mu = Reshape(reshape...)(mu)
-	xc := Sub(f.x, mu)
-	gx := MulC(2/float64(count), xc)
+	mu := Mean(f.Axes...)(f.x)       // mean(x, axes)
+	mu = Reshape(reshape...)(mu)     // reshape
+	xc := Sub(f.x, mu)               // x - mean(x, axes)
+	gx := MulC(2/float64(count), xc) // 2/N * (x - mean(x, axes))
 
 	gy0 := Reshape(reshape...)(gy[0])
 	return []*Variable{
