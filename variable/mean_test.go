182567c201dc170e5ab54caa06f88a60db9b814c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-28 19:07:33 +0900

    Add Mean

---
+++ variable/mean_test.go
@@ -0,0 +1,61 @@
+package variable_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/autograd/variable"
+)
+
+func ExampleMean() {
+	x := variable.New(
+		1, 2, 3,
+		4, 5, 6,
+	).Reshape(2, 3)
+
+	y := variable.Mean(0)(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable[3]([2.5 3.5 4.5])
+	// variable[2 3]([0.5 0.5 0.5 0.5 0.5 0.5])
+}
+
+func ExampleMean_axis01() {
+	x := variable.New(
+		1, 2, 3,
+		4, 5, 6,
+	).Reshape(2, 3)
+
+	y := variable.Mean(0, 1)(x)
+	y.Backward()
+
+	fmt.Println(y)
+	fmt.Println(variable.Clip(0, 0.1666)(x.Grad))
+
+	// Output:
+	// variable(3.5)
+	// variable[2 3]([0.1666 0.1666 0.1666 0.1666 0.1666 0.1666])
+}
+
+func ExampleMean_double() {
+	x := variable.New(
+		1, 2, 3,
+		4, 5, 6,
+	).Reshape(2, 3)
+
+	y := variable.Mean(0, 1)(x)
+	y.Backward(variable.Opts{CreateGraph: true})
+	fmt.Println(y)
+
+	gx := x.Grad
+	x.Cleargrad()
+	gx.Backward(variable.Opts{CreateGraph: true})
+	fmt.Println(x.Grad)
+
+	// Output:
+	// variable(3.5)
+	// <nil>
+}
